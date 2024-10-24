00d98ed637acb211071fca88b54f501ab3e737fd
Christopher Ooi <cooi@c2r4s1.42singapore.sg>
2024-10-24 14:25:30 +0800

    update

--- piscine_0/ex08/Loading.py
+++ piscine_0/ex08/Loading.py
@@ -13,7 +13,7 @@ def generate_bar(
     p = current / total
     fill_length = int(p * bar_length)
     bar = "=" * fill_length + "-" * (bar_length - fill_length)
-    return f"{p * 100:.2f}% [{bar}] {current}/{total} - {time_elapsed:.2f}s"
+    return f"{p * 100:.0f}%[{bar}] {current}/{total} - {time_elapsed:.2f}s"
 
 
 def ft_tqdm(lst: range) -> None:
@@ -24,21 +24,14 @@ def ft_tqdm(lst: range) -> None:
     current = 0
     # initial bar
     start = time()
-    print(generate_bar(current, total, 60, 0), end="\r")
+    print(generate_bar(current, total, 64, 0), end="\r")
 
     for i in lst:
         yield i
         current += 1
         time_elapsed = time() - start
-        print(generate_bar(current, total, 60, time_elapsed), end="\r")
+        print(generate_bar(current, total, 64, time_elapsed), end="\r")
 
-
-from time import sleep
-from tqdm import tqdm
 
 if __name__ == "__main__":
-    for i in ft_tqdm(range(100)):
-        sleep(0.005)
-    print()
-    for i in tqdm(range(100)):
-        sleep(0.005)
+    pass

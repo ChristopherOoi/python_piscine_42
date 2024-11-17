fa2a7cc9bf13e9253091f927dbfcf02d93314e24
ChristopherOoi <christopher_oo1@yahoo.com.sg>
2024-11-17 19:15:41 +0800

    update

--- piscine_1/ex04/rotate.py
+++ piscine_1/ex04/rotate.py
@@ -1,11 +1,13 @@
 from load_image import ft_load
 from matplotlib import pyplot as plt
+from numpy import array
 
 
 def main():
     """
     this main function loads the 'animal.jpeg' image
     prints some information about it, and slices it to 'zoom'
+    and rotates it
     then display the 'zoomed' image
     """
     path = "animal.jpeg"
@@ -20,16 +22,16 @@ def main():
         try:
             x = int(input("Enter x: "))
             y = int(input("Enter y: "))
-            w = int(input("Enter width: "))
-            h = int(input("Enter height: "))
+            s = int(input("Enter size: "))
             assert x >= 0 and y >= 0, "x and y must be positive"
-            assert w > 0 and h > 0, "width and height must be positive"
-            assert x + w <= image.shape[1], "width is too big"
-            assert y + h <= image.shape[0], "height is too big"
-            zoomed = image[y : y + h, x : x + w]
-            print("New shape after slicing: ", zoomed.shape)
-            print(zoomed)
-            plt.imshow(zoomed)
+            assert s > 0, "width and height must be positive"
+            assert x + s <= image.shape[1], "width is too big"
+            assert y + s <= image.shape[0], "height is too big"
+            zoomed = image[y : y + s, x : x + s]
+            transposed = array([list(col) for col in zip(*zoomed)])
+            print("New shape after rotation: ", transposed.shape)
+            print(transposed)
+            plt.imshow(transposed)
             plt.show()
         except Exception as e:
             print("Invalid input! ", e)

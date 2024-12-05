16a1b8949bb0ce66c3857f424f97b01b4a33daad
Christopher <christopher_oo1@yahoo.com.sg>
2024-12-05 11:09:18 +0800

    update

--- piscine_1/ex05/pimp_image.py
+++ piscine_1/ex05/pimp_image.py
@@ -1,4 +1,4 @@
-from numpy import mean
+from numpy import array, mean
 from load_image import ft_load
 from matplotlib import pyplot as plt
 
@@ -9,6 +9,7 @@ def ft_invert(array) -> array:
     """
     plt.imshow(255 - array)
     plt.show()
+    plt.savefig("inverted_image.jpg")
     return 255 - array
 
 
@@ -16,36 +17,49 @@ def ft_red(array) -> array:
     """
     Applies a red filter to the image received.
     """
-    plt.imshow(array[..., 0])
+    res = array.copy()
+    res[..., 1:] = 0
+    plt.imshow(res)
     plt.show()
-    return array[..., 0]
+    plt.savefig("red_image.jpg")
+    return res
 
 
 def ft_green(array) -> array:
     """
     Applies a green filter to the image received.
     """
-    plt.imshow(array[..., 1])
+    res = array.copy()
+    res[..., ::2] = 0
+    plt.imshow(res)
     plt.show()
-    return array[..., 1]
+    plt.savefig("green_image.jpg")
+    return res
 
 
 def ft_blue(array) -> array:
     """
     Applies a blue filter to the image received.
     """
-    plt.imshow(array[..., 2])
+    res = array.copy()
+    res[..., :2] = 0
+    plt.imshow(res)
     plt.show()
-    return array[..., 2]
+    plt.savefig("blue_image.jpg")
+    return res
 
 
 def ft_grey(array) -> array:
     """
     Turns the image received into a grayscale image.
     """
-    plt.imshow(mean(array, axis=2))
+    val = mean(array, axis=2)
+    res = array.copy()
+    res[..., :] = val.reshape(val.shape[0], val.shape[1], 1)
+    plt.imshow(res)
     plt.show()
-    return mean(array, axis=2)
+    plt.savefig("grey_image.jpg")
+    return res
 
 
 if __name__ == "__main__":
